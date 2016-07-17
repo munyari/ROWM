32e464a384cb9ab48e8aaf70584a45d6d52c42b1
Panashe M. Fundira <fundirap@gmail.com>
2016-07-17 13:03:18 -0400

    Simple example with wlc

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+extern crate rustwlc;
+use rustwlc::types::*;
+use rustwlc::callback;
+use rustwlc::WlcView;
+
+// Callback must be labelled extern as they will be called from C
+extern "C" fn view_created(view: WlcView) -> bool {
+    view.bring_to_front();
+    view.focus();
+    return true;
+}
+
+extern "C" fn view_focus(view: WlcView, focused: bool) {
+    view.set_state(VIEW_ACTIVATED, focused);
+}
+
 fn main() {
-    println!("Hello, world!");
+    callback::view_created(view_created);
+    callback::view_focus(view_focus);
+
+    // The default log handler will print wlc logs to stdout
+    rustwlc::log_set_default_handler();
+    let run_fn = rustwlc::init().expect("Unable to initalize!");
+    run_fn();
 }
